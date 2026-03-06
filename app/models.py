6c051cb2eb9f1b3bcc1a330d25b96148f2ebc96a
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-03-07 01:28:39 +0530

    notes table added '

--- app/models.py
+++ app/models.py
@@ -1,6 +1,17 @@
 from app import db
-class User(db.Model):
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    notes=db.relationship('Note', backref='user', lazy=True)
+
+class Note(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    title= db.Column(db.String(150), nullable=False)
+    content= db.Column(db.Text, nullable=False)
+    user_id= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
 
