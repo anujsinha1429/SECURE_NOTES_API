f75f099c23f2da731bdbab1c711c96aee89654f2
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-03-07 01:27:47 +0530

    login manager added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db= SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     
@@ -9,6 +11,11 @@ def create_app():
     app.config['SECRET_KEY'] = 'your_secret_key_here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
     db.init_app(app)
+    login_manager.init_app(app)
+    from app.models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
     from app.routes.auth import auth
     from app.routes.notes import notes 
     app.register_blueprint(auth)
