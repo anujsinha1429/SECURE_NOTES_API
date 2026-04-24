a04d89619af8b339d76c9f04775312110aef6cb5
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-04-24 19:37:40 +0530

    final fix for render port

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from app import create_app, db
 import os
+from app import create_app, db
 
-app= create_app()
+app = create_app()
 
 with app.app_context():
     db.create_all()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=port)
