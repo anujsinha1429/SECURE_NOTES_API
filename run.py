65a4cbf25edd82b3c2d9e78286a7b623597b9843
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-04-24 19:27:30 +0530

    fixed port binding

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import create_app, db
+import os
 
 app= create_app()
 
@@ -6,6 +7,5 @@ with app.app_context():
     db.create_all()
 
 if __name__=="__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=5000)
